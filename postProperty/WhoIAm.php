a3bf97134acbb8beed990fad9302cb6410639882
sushrusha <jadhavsush4@gmail.com>
2019-07-23 11:19:55 +0530

    - Added the post property dirctory:

---
+++ postProperty/WhoIAm.php
@@ -0,0 +1,1 @@
+<?php include 'propertymenubar.html'?>
---
+++ postProperty/listProperty.php
@@ -0,0 +1,34 @@
+
+<?php include 'propertymenubar.html'?>
+<div class="row" >
+    <!-- Box outline -->
+    <div class="box-outline mb-tb-5per">
+        <!-- Text property heading -->
+        <center><div class="mb-tb-5per">
+                    <label class="txtbox-property-heading back-color-blue white-font ">LIST PROPERTY FOR</label>
+                </div>
+        </center>
+        <!-- Types og property -->
+        <div class="row">
+            <div class="col-md-4">
+              <input type="radio" name="pg" value="pg"><span class="select-txt">PG</span>
+            </div>
+            <div class="col-md-4 center">
+              <input type="radio" name="pg" value="pg"><span class="select-txt">Flat</span>
+            </div>
+            <div class="col-md-4 center">
+              <input type="radio" name="pg" value="pg"><span class="select-txt">Entire building</span>
+            </div>
+        </div>
+      
+    </div> <!--box close-->
+    <div class="width-eighty m-auto">
+        <center>
+          <button class="btn-property back-color-yellow red-font">NEXT</button>
+        </center>
+    </div>
+
+</div>
+
+
+
