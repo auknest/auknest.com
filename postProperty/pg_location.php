4da6f8b7d58f357b40ccdbd212510d2a233b2a9c
sushrusha <jadhavsush4@gmail.com>
2019-07-29 14:18:21 +0530

    Added

---
+++ postProperty/pg_location.php
@@ -0,0 +1,43 @@
+<?php include 'propertymenubar.html'?>
+<div class="row" >
+    <!-- Box outline -->
+    <div class="box-outline mb-tb-5per">
+        <!-- Text property heading -->
+        <center><div class="mb-tb-5per">
+                    <label class="txtbox-property-heading back-color-blue white-font ">LOCATION</label>
+                </div>
+        </center>
+        <!-- Types og property -->
+        <div class="row">
+            <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-6">
+                    <select>
+                        <option selected disabled hidden>City</option>
+                        <option value="Pune" name="Pune"> Pune </option>
+                        <option value="Mumbai" name="Mumbai"> Mumbai </option>
+
+                    </select>
+                </div>
+                    <div class="col-md-6">
+                        <input type="textbox">
+                    </div>
+                </div>
+                <div class="col-md-12">
+                <input type="textbox">
+                </div>
+            </div>
+
+             </div>
+            <div class="col-md-6 center">
+            </div>
+        </div>
+      
+    </div> <!--box close-->
+    <div class="width-eighty m-auto">
+        <center>
+          <button class="btn-property back-color-yellow red-font">Save & Continue</button>
+        </center>
+    </div>
+
+</div>
---
+++ postProperty/pg_who_I .php
@@ -0,0 +1,30 @@
+<?php include 'propertymenubar.html'?>
+<div class="row" >
+    <!-- Box outline -->
+    <div class="box-outline mb-tb-5per">
+        <!-- Text property heading -->
+        <center><div class="mb-tb-5per">
+                    <label class="txtbox-property-heading back-color-blue white-font ">I AM</label>
+                </div>
+        </center>
+        <!-- Types og property -->
+        <div class="row">
+            <div class="col-md-4">
+              <input type="radio" name="pg-owner" value="pg-owner"><span class="select-txt">Owner</span>
+            </div>
+            <div class="col-md-4 center">
+              <input type="radio" name="pg-agent" value="pg-agent"><span class="select-txt">Agent / Broker</span>
+            </div>
+            <div class="col-md-4 center">
+              <input type="radio" name="pg-builder" value="pg-builder"><span class="select-txt">Builder</span>
+            </div>
+        </div>
+      
+    </div> <!--box close-->
+    <div class="width-eighty m-auto">
+        <center>
+          <button class="btn-property back-color-yellow red-font">Save & Continue</button>
+        </center>
+    </div>
+
+</div>
