d644d8100f267f6f6f99ed5fba4f52ca0c35d0bc
shogale-10 <52746595+shogale-10@users.noreply.github.com>
2019-07-30 11:46:54 +0530

    Updated

--- postProperty/pg_location.php
+++ postProperty/pg_location.php
@@ -1,39 +1,45 @@
 <?php include 'propertymenubar.html'?>
 <div class="row" >
     <!-- Box outline -->
-    <div class="box-outline mb-tb-5per">
+    <div class="box-outline-loc mb-tb-3per">
         <!-- Text property heading -->
         <center><div class="mb-tb-5per">
                     <label class="txtbox-property-heading back-color-blue white-font ">LOCATION</label>
                 </div>
         </center>
-        <!-- Types og property -->
+        <!-- Types pg property -->
         <div class="row">
-            <div class="col-md-6">
             <div class="row">
-                <div class="col-md-6">
-                    <select>
-                        <option selected disabled hidden>City</option>
-                        <option value="Pune" name="Pune"> Pune </option>
-                        <option value="Mumbai" name="Mumbai"> Mumbai </option>
-
-                    </select>
-                </div>
-                    <div class="col-md-6">
-                        <input type="textbox">
+                <div class="col-sm-12 col-md-5 col-lg-5">
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+                        <select>
+                            <option selected disabled hidden>City</option>
+                            <option value="Pune" name="Pune"> Pune </option>
+                            <option value="Mumbai" name="Mumbai"> Mumbai </option>
+                        </select>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+                        <input type="textbox" placeholder="Locality">
                     </div>
                 </div>
-                <div class="col-md-12">
-                <input type="textbox">
+                <div class="col-sm-12 col-md-7 col-lg-7">
+                    <input type="textbox" placeholder="Address">
+                </div>
+            </div> <!-- First row Ended-->
+            <div class="row">
+                <div class="col-sm-12 col-md-5 col-lg-5">
+                    <input type="textbox" placeholder="Project Name">
+                </div>
+                <div class="col-sm-12 col-md-7 col-lg-7">
+                    <input type="textbox" placeholder="MAP">
                 </div>
             </div>
+        </div> 
 
-             </div>
-            <div class="col-md-6 center">
-            </div>
-        </div>
+        
       
     </div> <!--box close-->
+
     <div class="width-eighty m-auto">
         <center>
           <button class="btn-property back-color-yellow red-font">Save & Continue</button>
