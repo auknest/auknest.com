7dc6743ad44c9cc26865e9acd966a4d717a54519
shogale-10 <52746595+shogale-10@users.noreply.github.com>
2019-10-15 12:26:47 +0530

    Page Created pgalldetails.php

---
+++ postProperty/pgalldetails.php
@@ -0,0 +1,447 @@
+<?php include 'propertymenubar.html'?>
+<style>
+    body{
+        background:#f3f4f9;
+    }
+</style>
+<div class="pd-1">
+    <br>
+    <div class="row">
+        <div class="col-sm-6 col-md-6 col-lg-6 back-color-white boxSh" >
+            <div class=" ma-0-5per ">
+                <div class="col-sm-3 col-md-3 col-lg-3 pd-3-5per">
+                    <span id="typeOf">1 BHK </span>PG
+                </div>
+                <!-- <hr style="width: 1px; height: 50px; background: black; border: none;" /> -->
+                <div class="col-sm-9 col-md-9 col-lg-9 pd-1" style="font-size:12px">
+                    <p class="mb-0">City : <span id="city">Pune<span></p>
+                    <p id="address" class="mb-0">AKURDI, NEAR KHANDOBA MANDIR</p>
+                    <p class="mb-0">Porject Name : <span id="project">Auknest<span></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-6 col-lg-6">
+            <div class="col-sm-4 col-md-4 col-lg-4 ">
+                <div class="back-color-white pd-1 center ma-0-5per boxSh bold pd-5per" >
+                    <span>Rent </span>
+                    <p><span id="rent">15,000</span>/- M</p>
+                </div>
+            </div>  
+            <div class="col-sm-4 col-md-4 col-lg-4">
+                <div class="back-color-white pd-1 center ma-0-5per boxSh bold pd-5per" >
+                    <span >Deposite </span>
+                    <p><span id="deposite">12,000</span>/-</p>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4 col-lg-4 ">
+                <div class="back-color-white pd-1 center ma-0-5per boxSh bold pd-5per" >
+                    <span >Available From </span>
+                    <p><span id="avlfrom">12/10/2019</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="pd-lr-1">
+    <div class="row">
+    <!-- Gallary Section -->
+    <div class="col-sm-6 col-md-6 col-lg-6 back-color-white">
+        <div class=" ma-0-5per">
+            <img src="../img/auknest.png" style="width:100%">
+            <div>
+                <img src="../img/auknest.png" style="width:24%">
+                <img src="../img/auknest.png" style="width:24%">
+                <img src="../img/auknest.png" style="width:24%">
+                <img src="../img/auknest.png" style="width:24%">
+            </div>
+        </div>
+    </div>
+    <!-- Features Section -->
+    <div class="col-sm-6 col-md-6 col-lg-6 ">
+        <div class="back-color-white ma-0-5per boxSh pd-1 ">
+            <span class="bold">FEATURES</span>
+            <div class="row"> 
+                <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div class="outline">
+                        <p class="mb-0 orange font-12">Preferred Tenant</p>
+                        <p class="mb-0 bold font-12" id="preferTo">Working Professional</p>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div class="outline">
+                        <p class="mb-0 orange font-12">Parking</p>
+                        <p class="mb-0 bold font-12" id="parking">Bike or Car</p>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div class="outline">
+                        <p class="mb-0 orange font-12">Avialable For</p>
+                        <p class="mb-0 bold font-12" id="avlFor">Girls or Boys</p>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div class="outline">
+                        <p class="mb-0 orange font-12">Posted on</p>
+                        <p class="mb-0 bold font-12" id="postedOn">2 Days Ago</p>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div class="outline">
+                        <p class="mb-0 orange font-12">Food</p>
+                        <p class="mb-0 bold font-12" id="food">Included</p>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div class="outline">
+                        <p class="mb-0 orange font-12">Furniture</p>
+                        <p class="mb-0 bold font-12" id="furnish">Semi Furnished</p>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div class="outline">
+                        <p class="mb-0 orange font-12">Posted by</p>
+                        <p class="mb-0 bold font-12" id="proPerson">Owner</p>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div class="outline">
+                        <p class="mb-0 orange font-12">Guardian Stay</p>
+                        <p class="mb-0 bold font-12" id="guardian">Allowed</p>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div class="outline">
+                        <p class="mb-0 orange font-12">Security Guard</p>
+                        <p class="mb-0 bold font-12" id="security">Yes</p>
+                    </div>
+                </div>
+            </div>
+            <div class="center ma-0-5per">
+                <button class="pd-2 connect_btn">CONNECT WITH OWNER</button>
+            </div>
+        </div>
+        <br>
+        <!-- Map Section -->
+        <div class="center ma-0-5per">
+            <img src="https://i.stack.imgur.com/AF16r.png" style="width:80%">
+        </div>
+    </div>
+    </div>
+</div>
+<!-- Extra Space Section -->
+<div class="row">
+    <div class="center">
+        <img src="../img/logo.png">
+    </div>
+    <div class="center" style="font-size: 30px" >
+        <span>EXTRA SPACE</span>
+    </div>
+</div>
+<br>
+<div class="row">
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Attached Bathroom</p>
+            <p class="mb-0 bold" id="bath">Yes</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Attached Balcony</p>
+            <p class="mb-0 bold" id="balcony">No</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Common Area</p>
+            <p class="mb-0 bold" id="area">Yes</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Parking</p>
+            <p class="mb-0 bold" id="parking">Both</p>
+        </div>
+    </div>
+</div>
+<br>
+<!-- PG Amenities Section -->
+<div class="row">
+    <div class="center">
+        <img src="../img/logo.png">
+    </div>
+    <div class="center" style="font-size: 30px" >
+        <span>PG AMENITIES</span>
+    </div>
+</div>
+<br>
+<!-- First Row -->
+<div class="row">
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">TV</p>
+            <p class="mb-0 bold" id="tv">Yes</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">DTH</p>
+            <p class="mb-0 bold" id="dth">No</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Sofa</p>
+            <p class="mb-0 bold" id="sofa">Yes</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Geizer</p>
+            <p class="mb-0 bold" id="giezer">Yes</p>
+        </div>
+    </div>
+</div>
+<!-- Secind Row -->
+<div class="row">
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Bed</p>
+            <p class="mb-0 bold" id="bed">Yes</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Locker</p>
+            <p class="mb-0 bold" id="locker">No</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">WiFi</p>
+            <p class="mb-0 bold" id="wifi">Yes</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Center Table</p>
+            <p class="mb-0 bold" id="centerTable">Yes</p>
+        </div>
+    </div>
+</div>
+<!-- Third Row -->
+<div class="row">
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">AC</p>
+            <p class="mb-0 bold" id="ac">Yes</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Freeze</p>
+            <p class="mb-0 bold" id="freeze">No</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Drinking Water</p>
+            <p class="mb-0 bold" id="drinkWater">Yes</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Room Cleaning</p>
+            <p class="mb-0 bold" id="cleaning">Yes</p>
+        </div>
+    </div>
+</div>
+<!-- Fourth Row -->
+<div class="row">
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Washing Machine</p>
+            <p class="mb-0 bold" id="washingMachine">Yes</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Gas (Cooking Allow)</p>
+            <p class="mb-0 bold" id="gas">No</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Cooler</p>
+            <p class="mb-0 bold" id="cooler">Yes</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Shoerack</p>
+            <p class="mb-0 bold" id="shoerack">Yes</p>
+        </div>
+    </div>
+</div>
+<!-- General & Safety Amenities Section -->
+<div class="row">
+    <div class="center">
+        <img src="../img/logo.png">
+    </div>
+    <div class="center" style="font-size: 30px" >
+        <span>GENERAL & SAFETY AMENITIES</span>
+    </div>
+</div>
+<br>
+<!-- First Row -->
+<div class="row">
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Lift</p>
+            <p class="mb-0 bold" id="lift">Yes</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">CCTV</p>
+            <p class="mb-0 bold" id="cctv">No</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Power Backup</p>
+            <p class="mb-0 bold" id="powerBackup">Yes</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Water Supply</p>
+            <p class="mb-0 bold" id="waterSupply">Corporation</p>
+        </div>
+    </div>
+</div>
+<!-- Second Row -->
+<div class="row">
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Security Guard</p>
+            <p class="mb-0 bold" id="security">Yes</p>
+        </div>
+    </div>
+</div>
+<!-- Property Discussion -->
+<div class="row">
+    <div class="center">
+        <img src="../img/logo.png">
+    </div>
+    <div class="center" style="font-size: 30px" >
+        <span>PROPERTY DISCUSSION</span>
+    </div>
+</div>
+<br>
+<!-- First Row -->
+<div class="row">
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Total Floor</p>
+            <p class="mb-0 bold" id="totalFloor">4</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Floor Number</p>
+            <p class="mb-0 bold" id="floorNumber">3</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Total Rooms</p>
+            <p class="mb-0 bold" id="totalRooms">2</p>
+        </div>
+    </div>
+    <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+        <div class="back-color-white ma-1-5per pd-5per">
+            <p class="mb-0 orange ">Gate Closing Time</p>
+            <p class="mb-0 bold" id="gateCloseTime">9 pm</p>
+        </div>
+    </div>
+</div>
+<!-- Property Description  -->
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="back-color-white ma-1-5per" >
+            <div class="pd-2">
+                <p class="orange">
+                    <span>Describe Your Property</span>
+                </p>
+                <p id="desc">
+                    Describe Your Property details in Short.........
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<!-- Button Section -->
+<div class="row ">
+        <div class="hidden-xs col-sm-3 col-md-3 col-lg-3"></div>
+        <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3 center">
+            <div class="w-100per center red-font back-color-yellow pd-2">
+                <a href="#">
+                    <p id="ajax-edit" class="red-font" >Edit <br></p>
+                </a>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3 center">
+            <div class="w-100per center red-font back-color-yellow pd-2">
+                <a href="listProperty.php">
+                    <p class="red-font">Submit</p>
+                </a>
+            </div>  
+        </div>
+        <div class="hidden-xs col-sm-3 col-md-3 col-lg-3"></div>
+</div>
+<br>
+<!-- Recomended Property Section -->
+<script>
+
+
+    // $('#ajax-edit').click(function(e) {
+        $(document).ready(function(){   
+        console.log(sessionStorage.getItem("pro_id"));
+    
+    $.ajax ({
+        url: "http://localhost:3000/get_profileProdetails?pro_id="+sessionStorage.getItem("pro_id")+"&pro_type="+sessionStorage.getItem("pro_type"),
+        type:"GET",
+        data: {
+            // "pro_id": sessionStorage.getItem("pro_id"),
+            // "pro_type": sessionStorage.getItem("pro_type")
+        },
+        success: function (data) {
+                console.log(data);
+                    $('#pro_type').html(data[0].pro_type);
+                    $('#pg_for').html(data[0].pg_for);
+                    $('#room_type').html(data[0].pg_room_type);
+                    $('#rent').html(data[0].expected_rent);
+                    $('#avlfrom').html(data[0].avl_frm);
+                    $('#deposite').html(data[0].security_amt);
+                    $('#pg_available').html(data[0].pg_available);
+                    $('#food_included').html(data[0].food_included);
+                    // $('.profile_img').css({'background':"url(../WebAPI/uploads/" + data[0].profile_img + ")"});
+                    var key=Object.keys(data[0].profile_img);
+                    console.log("img....", data[0].profile_img[key]);
+                    $('.profile_img').attr("src","../WebAPI/uploads/" + data[0].profile_img[key]+ "");
+
+                        // alert(data);
+                    },
+            error: function() {
+                  console.log('Error In AJAX...');
+                },
+
+    });
+    $('#ajax-edit').click(function(e) {
+            e.preventDefault();
+                     window.location.href = "./listProperty.php?pro_id="+sessionStorage.getItem("pro_id");
+                });
+});
+    </script>
